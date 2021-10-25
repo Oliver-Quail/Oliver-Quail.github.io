71f8cc072e479d8152a935b63dad5621f382940b
Ollie-Q <82865751+Ollie-Q@users.noreply.github.com>
2021-10-25 18:30:08 +1100

    Adding js parallax

---
+++ Javascript/DataIntro2.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var ParaBackground1 = document.getElementById("ParaBackground1");
+    var ParaBackground2 = document.getElementById("ParaBackground2");
+    var ParaBackground3 = document.getElementById("ParaBackground3");
+    var ParaBackground4 = document.getElementById("ParaBackground4");
+    var ParaBackgrounds = document.getElementsByClassName("ArticleParaBackground");
+    new simpleParallax(ParaBackgrounds);
+
+
+}
