6d0b0ec7f8f94f937e9900adbac9c597ea46b400
Ollie-Q <82865751+Ollie-Q@users.noreply.github.com>
2022-08-09 17:13:38 +1000

    Removed php

--- SAT2/JavaScript/index.js
+++ SAT2/JavaScript/index.js
@@ -10,19 +10,7 @@ window.onload = function() {
             alert("You have left some required form fields blank or have entered invalid data");
         }
         else {
-            var xhttp =  new XMLHttpRequest();
-            xhttp.open("POST", "API.php?Mode=Login", true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.send("Email="+encodeURIComponent(Email.value)+"&Password="+ encodeURIComponent(Password.value));   
-            xhttp.onload = function() {
-                alert(this.responseText);
-                if(this.responseText == "Login success") {
-                    window.location = "Home.html";
-                }
-                else {
-                    alert("invalid email or password");
-                }
-            }        
+            window.location = 'Menu.html';
         }
     }
 
