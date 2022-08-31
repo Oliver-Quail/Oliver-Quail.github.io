e768b51111e411d2c778d060edb3b7b9b8f8a8dc
Ollie-Q <82865751+Ollie-Q@users.noreply.github.com>
2022-08-31 18:36:31 +1000

    Improved index2

--- SAT2/JavaScript/index.js
+++ SAT2/JavaScript/index.js
@@ -5,7 +5,6 @@ window.onload = function() {
     var Password = document.getElementById("PasswordInput");
 
     LoginButton.onclick = function() {
-        alert("clicked");
         if(Email.value == null || Password.value == null || !Email.value.includes("@")) {
             alert("You have left some required form fields blank or have entered invalid data");
         }
