4a9af4c9607d42a758d952206440e95fdd4fdabb
Ollie-Q <82865751+Ollie-Q@users.noreply.github.com>
2022-10-01 09:40:15 +1000

    Added image slider

--- Javascript/Index2.js
+++ Javascript/Index2.js
@@ -24,5 +24,38 @@ window.onload = function() {
     for(var Counter = 0; Counter < L_Menu.length; Counter++) {
         L_Menu[Counter].addEventListener("click", LinkFunction, false);
     }
+    showSlides(slideIndex);
+}
 
+
+
+
+    let slideIndex = 1;
+
+    // Next/previous controls
+    function plusSlides(n) {
+        console.log("yeah");
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+        
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        //dots[slideIndex-1].className += " active";
 }
--- SAT/test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    require("loader.php");
-
-    $Controller = new Controller();
-
-    $Controller->C_CreateChat("Oliver Quail", "Oliver2");
-?>
