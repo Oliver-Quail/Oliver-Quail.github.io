95011ab81dfe918672ebd8c1c5017428e51d2657
Ollie-Q <82865751+Ollie-Q@users.noreply.github.com>
2022-12-23 11:58:48 +1100

    new index build 1

---
+++ Javascript/index3.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    var Roles = document.getElementById("Roles");
+    var RolesText = ["Web Developer", "Programmer", "Graphic Designer"];
+    var RolesOutputText = "";
+    var CounterLength = 0;
+    var Counter = 0;
+    var RolesTimer = setInterval(() => {
+        if(CounterLength < RolesText[Counter].length) {
+            RolesOutputText += RolesText[Counter][CounterLength];
+            Roles.innerHTML = RolesOutputText;
+            CounterLength++;
+        }
+        else if (Counter == 2) {
+            clearInterval(RolesTimer);
+        }
+        else {
+            RolesOutputText += "<br/>";
+            CounterLength = 0;
+            Counter++;
+        }
+
+    }, 200);
+
+}
