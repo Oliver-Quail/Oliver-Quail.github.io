fab9a151297dab42553558e2c944f528e24520fa
Ollie-Q <82865751+Ollie-Q@users.noreply.github.com>
2022-12-23 16:23:39 +1100

    More updates to new index

--- Javascript/index3.js
+++ Javascript/index3.js
@@ -18,7 +18,20 @@ window.onload = function() {
             CounterLength = 0;
             Counter++;
         }
-
     }, 200);
+
+    var MenuButton = document.getElementById("MenuButton");
+    var Menu = document.getElementById("MenuBar");
+    var MenuShown = 0;
+    MenuButton.addEventListener("click", function(){
+        if(MenuShown == 0) {
+            MenuShown = 1;
+            Menu.classList.add("ShowMenu");
+        }
+        else {
+            MenuShown = 0;
+            Menu.classList.remove("ShowMenu");
+        }
+    });
 
 }
