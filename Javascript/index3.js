22c733df94d9062cf13a895cab0c55bc8c581acc
Ollie-Q <oliver.quail3@gmail.com>
2022-12-28 13:00:01 +1100

    Fixed Javascript change thing

--- Javascript/index3.js
+++ Javascript/index3.js
@@ -43,7 +43,6 @@ function CreateFluxList(TitlesHolder, ContentHolder) {
     var TitlesArray = TitlesHolder.children;
     var ContentsArray = ContentHolder.children;
     console.log(TitlesArray);
-    console.log(TitlesArray[1].tagName);
     console.log(TitlesArray.length);
     if(TitlesArray.length != ContentsArray.length) {
         console.error("The number of Titles does not match the number of contents pannels");
@@ -52,24 +51,21 @@ function CreateFluxList(TitlesHolder, ContentHolder) {
 
     var Counter = 0;
     console.log("--------");
-    while(Counter < (TitlesArray.length - 1)) {
-        console.log( TitlesArray[Counter]);
-        TitlesArray[Counter].addEventListener("click", function() {
-            console.log(Counter);
-            console.log(TitlesArray[Counter]);
-            //if(TitlesArray[Counter].tagName == "p"){
-                var ContentCounter = 0;
-                while(ContentCounter < (ContentsArray.length - 1)) {
-                    ContentsArray[ContentCounter].removeAttribute("class");
-                    TitlesArray[ContentCounter].removeAttribute("class");
-                    ContentCounter++;
-                }
-                console.log("-------b");
-                ContentsArray[Counter].classList.add("ActiveSkillText");
-                TitlesArray[Counter].classList.add("ActiveSkill");
-           // }
+
+    Object.keys(TitlesArray).forEach(Key => {
+        console.log(TitlesArray[Key]);
+        var CurrentPanel =  ContentsArray[Key];
+
+        TitlesArray[Key].addEventListener("click", function(){
+            var ContentCounter = 0;
+            while(ContentCounter < ContentsArray.length) {
+                ContentsArray[ContentCounter].removeAttribute("class");
+                TitlesArray[ContentCounter].removeAttribute("class");
+                ContentCounter++;
+            }
+            CurrentPanel.classList.add("ActiveSkillText");
+            this.classList.add("ActiveSkill");
         });
-        Counter++;
-    }
-    
+        
+    });
 }
